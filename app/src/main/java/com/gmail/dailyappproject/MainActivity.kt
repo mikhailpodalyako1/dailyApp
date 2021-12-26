d5d79c46a8d14ec7256cfffda12df22bc25b14e5
Mikhail Podalyako <mihailpodalyako1@gmail.com>
2021-12-26 13:57:42 +0300

    Старт проекта

---
+++ app/src/main/java/com/gmail/dailyappproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gmail.dailyappproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gmail.dailyappproject.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/gmail/dailyappproject/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.gmail.dailyappproject.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
